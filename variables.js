fe3a3bfef7cc20338a9e57946f849d7f08aeae10
user.name:"Mulubwa17 <user.email:"mulubwac@gmail.com>
2019-06-15 16:03:47 +0200

    answers for js phase 1

--- variables.js
+++ variables.js
@@ -1,1 +1,13 @@
 // you solution should be here
+var x = 1984;
+var y = 2016;
+var age = y - x;
+console.log('They are either ' + age + ' or ' + (age - 1));
+
+//2
+var age = 28;
+var maxAge = 100;
+var numPerDay = 2;
+var totalNeeded = (numPerDay * 365) * (maxAge - age);
+var message = 'You will need ' + totalNeeded + ' cups of tea to last you until the ripe old age of ' + maxAge;
+console.log(message);
